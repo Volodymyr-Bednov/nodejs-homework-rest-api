e05939b7c4338a01b088af267e25b701bf96b7b4
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-04-25 15:19:42 +0300

    hw02

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -1,9 +1,7 @@
 const addContact = require("./addContact");
 const getContactById = require("./getContactById");
-
 const listContacts = require("./listContacts");
 const removeContact = require("./removeContact");
-
 const updateContact = require("./updateContact");
 
 module.exports = {
